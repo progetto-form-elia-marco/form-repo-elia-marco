767a4804d557da6cb69ab562f62d33a3bc4e68b0
MarcoCarpo <marco.carpo95@gmail.com>
2021-04-29 17:09:03 +0200

    fixato nome email in welcome

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -3,15 +3,17 @@ import "./css/welcome.css";
 import { Link } from "react-router-dom";
 
 class Welcome extends Component {
-
+  modifyWelcomeName = () => {
+    const email = JSON.parse(localStorage.getItem("attivo")).email;
+    const [name, emailAt] = email.split("@");
+    return name;
+  };
 
   render() {
     return (
       <div className="welcomeContainer">
         <div className="welcomeCard">
-          <h1 className="welcomeTitle">
-            Welcome {JSON.parse(localStorage.getItem("attivo")).email}!
-          </h1>
+          <h1 className="welcomeTitle">Welcome {this.modifyWelcomeName()}!</h1>
           <div className="welcomeLinks">
             <Link
               to="/"
