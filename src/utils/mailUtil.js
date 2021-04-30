a5860a4df78dd6e6f9bcc4597e11b48445852243
LAPTOP-PPJ87ETK\Elia Minozzi <eliaminozzi5@gmail.it>
2021-04-30 10:32:28 +0200

    Add UTILS

---
+++ src/utils/mailUtil.js
@@ -0,0 +1,13 @@
+const UTILS= {
+
+    emailValidation: (email) => {
+
+        const control = /^[^A-Z0-9._%+-]+@[^A-Z0-9.-]+\.[^A-Z]{2,3}$/;
+        return control.test(email)
+    }
+
+}
+
+export {
+    UTILS,
+};
